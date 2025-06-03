b22191ea64a2b521cb37078f466c8318ed2533b0
newmaster <2338946313@qq.com>
2025-06-04 00:47:41 +0800

    更新CourseBaseInfoController,添加了Sl4j注解与content公共URL路径

--- content-service/src/main/java/com/study/online/controller/CourseBaseInfoController.java
+++ content-service/src/main/java/com/study/online/controller/CourseBaseInfoController.java
@@ -4,11 +4,14 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.study.online.model.PageParams;
 import com.study.online.model.dto.QueryCourseParamsDTO;
 import com.study.online.model.po.CourseBase;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
+@RequestMapping("/content")
 public class CourseBaseInfoController {
 	
 	@RequestMapping("/course/list")
