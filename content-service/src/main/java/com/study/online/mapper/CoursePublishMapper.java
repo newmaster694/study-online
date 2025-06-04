b27ccd4b90dc44bdcd037f96a3b01337371afb36
newmaster <2338946313@qq.com>
2025-06-04 14:32:24 +0800

    生成content-service模块的Mapper接口

---
+++ content-service/src/main/java/com/study/online/mapper/CoursePublishMapper.java
@@ -0,0 +1,15 @@
+package com.study.online.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.study.online.model.po.CoursePublish;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>课程发布 Mapper 接口</p>
+ *
+ * @author newmaster
+ * @since 2025-06-04
+ */
+@Mapper
+public interface CoursePublishMapper extends BaseMapper<CoursePublish> {
+}
