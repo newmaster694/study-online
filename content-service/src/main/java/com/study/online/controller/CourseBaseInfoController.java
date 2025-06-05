2a32672bd53f1fd50e8c41025af82ad9ac227b24
newmaster <2338946313@qq.com>
2025-06-05 19:55:41 +0800

    content-service:定义了查询课程分类树模型的HTTP API

--- content-service/src/main/java/com/study/online/controller/CourseBaseInfoController.java
+++ content-service/src/main/java/com/study/online/controller/CourseBaseInfoController.java
@@ -2,27 +2,32 @@ package com.study.online.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.study.online.model.PageParams;
+import com.study.online.model.dto.CourseCategoryTreeDTO;
 import com.study.online.model.dto.QueryCourseParamsDTO;
 import com.study.online.model.po.CourseBase;
 import com.study.online.service.ICourseBaseInfoService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("/content")
 public class CourseBaseInfoController {
-	
+
 	@Resource
 	private ICourseBaseInfoService courseBaseInfoService;
-	
+
 	@PostMapping("/course/list")
 	public Page<CourseBase> list(PageParams pageParams,
 			@RequestBody QueryCourseParamsDTO queryCourseParamsDTO) {
 		return courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDTO);
+	}
+
+	@GetMapping("/course-category/tree-nodes")
+	public List<CourseCategoryTreeDTO> queryTreeNodes() {
+		return null;
 	}
 }
