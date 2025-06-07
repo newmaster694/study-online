f1acba0a89f600f712ead32d929b87bc45c59d3c
newmaster <2338946313@qq.com>
2025-06-07 20:10:55 +0800

    完善了controller层的一些注释信息

--- content-service/src/main/java/com/study/online/content/controller/CourseBaseInfoController.java
+++ content-service/src/main/java/com/study/online/content/controller/CourseBaseInfoController.java
@@ -28,22 +28,52 @@ public class CourseBaseInfoController {
 	@Resource
 	private ICourseCategoryService courseCategoryService;
 
+	/**
+	 * <p>查询课程列表（分页）接口</p>
+	 *
+	 * @param pageParams           基础分页查询参数
+	 * @param queryCourseParamsDTO 课程分页查询参数
+	 * @return Mybatis-Plus->Page(CourseBase)
+	 */
 	@PostMapping("/course/list")
-	public Page<CourseBase> list(PageParams pageParams,
+	public Page<CourseBase> list(
+			PageParams pageParams,
 			@RequestBody(required = false) QueryCourseParamsDTO queryCourseParamsDTO) {
 		return courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDTO);
 	}
 
+	/**
+	 * <p>查询课程分类树节点接口</p>
+	 *
+	 * @return List(CourseCategoryTreeDTO)
+	 */
 	@GetMapping("/course-category/tree-nodes")
 	public List<CourseCategoryTreeDTO> queryTreeNodes() {
 		return courseCategoryService.queryTreeNodes("1");
 	}
 
+	/**
+	 * <p>创建课程接口</p>
+	 *
+	 * @param addCourseDTO 创建课程信息
+	 * @return 课程基础信息&课程营销信息
+	 */
 	@PostMapping("/course")
 	public CourseBaseInfoDTO createCourseBase(
-		@RequestBody @Validated({ValidationGroup.Inster.class}) AddCourseDTO addCourseDTO) {
+			@RequestBody @Validated({ValidationGroup.Inster.class}) AddCourseDTO addCourseDTO) {
 		//机构ID，由于认证系统还未上线，先硬编码
 		Long companyId = 1232141425L;
 		return courseBaseInfoService.createCourseBase(companyId, addCourseDTO);
+	}
+
+	/**
+	 * <p>根据id查询课程信息</p>
+	 *
+	 * @param courseId 课程id
+	 * @return 课程基础信息&课程营销信息
+	 */
+	@GetMapping("/course/{courseId}")
+	public CourseBaseInfoDTO getCourseBaseById(@PathVariable Long courseId) {
+		return null;
 	}
 }
