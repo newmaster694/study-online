ae210a553894831b9ebb17277d4fbc7d2dbe7699
newmaster <2338946313@qq.com>
2025-06-16 22:40:56 +0800

    feat(media-service): 添加课程文件上传相关接口
    - 新增课程文件上传接口，支持图片等文件的上传
    - 添加文件上传前检查、分块文件上传前检查和上传分块文件等接口
    - 优化了上传接口的路由，统一到 /upload 前缀下

--- media-service/src/main/java/study/online/media/controller/UploadController.java
+++ media-service/src/main/java/study/online/media/controller/UploadController.java
@@ -3,30 +3,29 @@ package study.online.media.controller;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import study.online.base.model.RestResponse;
 import study.online.media.model.dto.UploadFileParamsDTO;
 import study.online.media.model.dto.UploadFileResultDTO;
 import study.online.media.service.IUploadService;
 
 @RestController
 @Slf4j
+@RequestMapping("/upload")
 public class UploadController {
 
 	@Resource
 	private IUploadService uploadService;
 
 	/**
-	 * 上传文件接口
+	 * 上传课程相关（图片）文件接口
 	 *
 	 * @param file 文件
 	 * @return UploadFileResultDTO
 	 */
-	@PostMapping(value = "/upload/coursefile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public UploadFileResultDTO upload(
-		@RequestPart("filedata") MultipartFile file) {
+	@PostMapping(value = "/coursefile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public UploadFileResultDTO upload(@RequestPart("filedata") MultipartFile file) {
 
 		Long companyId = 1232141425L;
 
@@ -37,5 +36,48 @@ public class UploadController {
 			.setFilename(file.getOriginalFilename());
 
 		return uploadService.uploadFile(companyId, uploadFileParamsDTO, file);
+	}
+
+	/**
+	 * 文件上传前检查文件接口
+	 *
+	 * @param fileMD5 上传文件的md5值
+	 * @return {@code  RestResponse<Boolean>}
+	 */
+	@PostMapping("/checkfile")
+	public RestResponse<Boolean> checkFile(@RequestParam("fileMd5") String fileMD5) {
+		return null;
+	}
+
+	/**
+	 * 分块文件上传前的检测接口
+	 *
+	 * @param fileMD5 上传文件的md5值
+	 * @param chunk   分块文件的序号
+	 * @return {@code RestResponse<Boolean>}
+	 */
+	@PostMapping("/checkchunk")
+	public RestResponse<Boolean> checkChunk(
+		@RequestParam("fileMd5") String fileMD5,
+		@RequestParam("chunk") int chunk
+	) {
+		return null;
+	}
+
+	/**
+	 * 上传分块文件接口
+	 *
+	 * @param file 上传的分块文件
+	 * @param fileMD5 文件的MD5值
+	 * @param chunk 分块文件的序号
+	 * @return {@code RestResponse<Object>}
+	 */
+	@PostMapping("/uploadchunk")
+	public RestResponse<Object> uploadchunk(
+		@RequestParam("file") MultipartFile file,
+		@RequestParam("fileMd5") String fileMD5,
+		@RequestParam("chunk") int chunk
+		) {
+		return null;
 	}
 }
