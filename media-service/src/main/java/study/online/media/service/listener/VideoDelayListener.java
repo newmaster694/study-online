0f3bb307f475d374d8e3a57eccd696e9c8aded02
newmaster <2338946313@qq.com>
2025-07-19 17:21:37 +0800

    refactor(media-service): 优化 Redis 扫描和删除键的逻辑
    - 移除了 ScanOptions 和 Cursor 的使用，简化了代码结构
    - 使用 redisTemplate.keys()直接获取匹配的键集合，提高了代码可读性
    - 使用 redisTemplate.delete() 直接删除键集合，提升了代码执行效率- 减少了冗余的异常捕获和日志记录，使代码更加简洁

--- media-service/src/main/java/study/online/media/service/listener/VideoDelayListener.java
+++ media-service/src/main/java/study/online/media/service/listener/VideoDelayListener.java
@@ -6,8 +6,6 @@ import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.data.redis.core.Cursor;
-import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import study.online.base.constant.MQConstant;
@@ -15,8 +13,7 @@ import study.online.media.model.po.MediaProcess;
 import study.online.media.service.IMediaProcessService;
 import study.online.media.service.impl.FileService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Component
@@ -47,20 +44,10 @@ public class VideoDelayListener {
 		key = MQConstant.FILE_INTERRUPT_KEY))
 	public void interruptFileHandler(String fileMd5) {
 		String pattern = "chunk:" + fileMd5 + ":*";
-		ScanOptions options = ScanOptions.scanOptions().match(pattern).build();
 
-		List<byte[]> keysToDelete = new ArrayList<>();
-		try {
-			Cursor<byte[]> cursor = redisTemplate.getConnectionFactory().getConnection().scan(options);
-			while (cursor.hasNext()) {
-				keysToDelete.add(cursor.next());
-			}
-		} catch (Exception e) {
-			log.error("redis删除异常-{}", e.getMessage());
-		}
-
-		if (!keysToDelete.isEmpty()) {
-			redisTemplate.getConnectionFactory().getConnection().del(keysToDelete.toArray(new byte[0][]));
+		Set<String> keys = redisTemplate.keys(pattern);
+		if (!keys.isEmpty()) {
+			redisTemplate.delete(keys);
 			fileService.clearChunkFiles(fileService.getChunkFileFolderPath(fileMd5));
 		}
 	}
