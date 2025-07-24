0742293543238604f5c9f8c2a822f3e57809fccb
newmaster <2338946313@qq.com>
2025-07-24 19:51:03 +0800

    refactor(content-service, media-service): 移除 Spring 配置导入
    - 从 ContentApplication 和 MediaApplication 中移除了 @Import(SpringConfig.class)
    - 更新了 @SpringBootApplication 注解以包含 "study.online.base" 包
    - 这些更改简化了 Spring 配置结构，并消除了冗余的配置导入

--- content-service/src/main/java/study/online/content/ContentApplication.java
+++ content-service/src/main/java/study/online/content/ContentApplication.java
@@ -4,13 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import study.online.base.config.SpringConfig;
 
 
 @Slf4j
-@Import(SpringConfig.class)
 @EnableTransactionManagement
 @MapperScan("study.online.content.mapper")
 @SpringBootApplication(scanBasePackages = {"study.online.content", "study.online.messagesdk", "study.online.base"})
--- media-service/src/main/java/study/online/media/MediaApplication.java
+++ media-service/src/main/java/study/online/media/MediaApplication.java
@@ -4,12 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import study.online.base.config.SpringConfig;
 
 @Slf4j
-@Import(SpringConfig.class)
 @EnableTransactionManagement
 @EnableAspectJAutoProxy(exposeProxy = true) //暴露代理对象注解
 @SpringBootApplication(scanBasePackages = {"study.online", "study.online.base"})
